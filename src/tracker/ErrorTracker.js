12f7ebd7384da4c0ae4318d5fe39d50eccfd3d65
Maido Käära <maidokaara@gmail.com>
2018-05-22 21:28:20 +0300

    Separate Sentry config for landing

--- src/tracker/ErrorTracker.js
+++ src/tracker/ErrorTracker.js
@@ -2,10 +2,10 @@
 
 import Raven from 'raven-js';
 
-export function initialize():void {
+export function initialize(): void {
   if (process.env.NODE_ENV === 'production') {
     Raven
-      .config('https://171315bead0b435284d807e00caa2a0d@sentry.io/227741', {
+      .config('https://43a9fed17500493eb47f471d475136dc@sentry.io/1211513', {
         environment: process.env.NODE_ENV,
       })
       .install();
