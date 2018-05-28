4d1e823498fc6672668638e63886d3860179ac04
fpsbernardo <fpsbernardo@gmail.com>
2018-05-28 23:55:25 +0300

    updated links to app

--- src/landing/PriceComparisonTable.js
+++ src/landing/PriceComparisonTable.js
@@ -61,13 +61,13 @@ class PriceComparisonTable extends React.Component<Props> {
               <tr>
                 <Th scope="col" />
                 <Th scope="col">
-                  <Image width="75px" src={change}/>
+                  <Image width="75px" src={change} />
                 </Th>
                 <Th scope="col">
-                  <Image width="75px" src={coinbase}/>
+                  <Image width="75px" src={coinbase} />
                 </Th>
                 <Th scope="col">
-                  <Image width="65px" src={etoro}/>
+                  <Image width="65px" src={etoro} />
                 </Th>
               </tr>
               </thead>
